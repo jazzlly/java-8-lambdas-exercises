6ef7bb5139671aeb963cfe14004a5790b0a1c6d0
Ray Jiang <jiangrui.tr2008@gmail.com>
2017-07-03 09:13:40 +0800

    add answers

--- src/main/java/com/insightfullogic/java8/exercises/chapter3/Question1.java
+++ src/main/java/com/insightfullogic/java8/exercises/chapter3/Question1.java
@@ -4,21 +4,57 @@ import com.insightfullogic.java8.examples.chapter1.Album;
 import com.insightfullogic.java8.examples.chapter1.Artist;
 import com.insightfullogic.java8.exercises.Exercises;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
 
 public class Question1 {
     public static int addUp(Stream<Integer> numbers) {
-        return Exercises.replaceThisWithSolution();
+        return numbers.reduce(0, (acc, element) -> acc + element);
+        // return Exercises.replaceThisWithSolution();
     }
 
     public static List<String> getNamesAndOrigins(List<Artist> artists) {
-        return Exercises.replaceThisWithSolution();
+        return artists.stream()
+                .map(artist -> Stream.of(artist.getName(), artist.getNationality()))
+                .flatMap(stringStream -> stringStream)
+                .collect(toList());
+        // return Exercises.replaceThisWithSolution();
     }
 
     public static List<Album> getAlbumsWithAtMostThreeTracks(List<Album> input) {
-        return Exercises.replaceThisWithSolution();
+        return input.stream()
+                .filter(album -> album.getTrackList() != null && album.getTrackList().size() <= 3)
+                .collect(toList());
+       // return Exercises.replaceThisWithSolution();
+    }
+
+
+    public static long getLowcaseCharsNum(String str) {
+        return str.chars().filter(value -> Character.isLowerCase(value)).count();
+    }
+
+    public static String getLowcaseMaxString(List<String> strings) {
+        String tmp = "";
+        strings.stream().filter(s -> {
+
+        })
+    }
+
+    public static void main(String[] args) {
+        System.out.println(addUp(Stream.of(1,2,3)));
+
+        System.out.println(
+                getNamesAndOrigins(
+                        Stream.of(
+                                new Artist("a1", "n1"),
+                                new Artist("a2", "n2")).collect(toList())));
+
+        System.out.println(getLowcaseCharsNum("Hello, World"));
+
     }
 }
--- src/main/java/com/insightfullogic/java8/exercises/chapter3/Question2.java
+++ src/main/java/com/insightfullogic/java8/exercises/chapter3/Question2.java
@@ -1,13 +1,22 @@
 package com.insightfullogic.java8.exercises.chapter3;
 
 import com.insightfullogic.java8.examples.chapter1.Artist;
+import com.insightfullogic.java8.examples.chapter1.SampleData;
 import com.insightfullogic.java8.exercises.Exercises;
 
 import java.util.List;
 
 public class Question2 {
     // Q3
-    public static int countBandMembersInternal(List<Artist> artists) {
-        return Exercises.replaceThisWithSolution();
+    public static long countBandMembersInternal(List<Artist> artists) {
+        return artists.stream().map(artist -> artist.getMembers().count())
+                .reduce(0l, (aLong, aLong2) -> aLong + aLong2);
+        // return Exercises.replaceThisWithSolution();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(
+                countBandMembersInternal(SampleData.getThreeArtists())
+        );
     }
 }
